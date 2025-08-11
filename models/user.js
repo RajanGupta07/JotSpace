a5cbec3cb43681d19fc3331583809eadcf5fc01e
rajangupta07 <user.email like: "Rajan kumar gupta>
2025-08-11 12:09:53 +0530

    mongoose change

--- models/user.js
+++ models/user.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 const userSchema = mongoose.Schema({
   username: String,
